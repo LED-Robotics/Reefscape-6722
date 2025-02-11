840d4dddc2091494edd6ef3a4ffc4470f6749915
boltsforcup <robertlasch07@gmail.com>
2025-02-10 22:03:54 -0500

    Path following

--- src/main/include/subsystems/DriveSubsystem/Constants.h
+++ src/main/include/subsystems/DriveSubsystem/Constants.h
@@ -39,7 +39,7 @@ namespace DriveConstants {
     
     // info for odometry
     constexpr auto kDriveBaseRadius = 0.319786_m;
-    constexpr double kWheelRadius = .0508;
+    constexpr double kWheelRadius = .0512;
     constexpr double kDriveRatio = 1 / 4.41;
     constexpr double kTurnRatio = 1 / 11.3142;
     constexpr double kDriveDistancePerRev = (2 * std::numbers::pi * kWheelRadius) * kDriveRatio;
@@ -62,8 +62,8 @@ namespace DriveConstants {
     constexpr double kDriveDeadzone = 0.2;
     constexpr double kTurnDeadzone = 0.1;
     constexpr double kDriveCurveExtent = 0.6;
-    constexpr auto kDriveAccelerationLimit = 9.0_mps_sq;
-    constexpr auto kDriveDecelerationLimit = 25.0_mps_sq;
+    constexpr auto kDriveAccelerationLimit = 6.0_mps_sq;
+    constexpr auto kDriveDecelerationLimit = 6.0_mps_sq;
     constexpr auto kDriveTranslationLimit = 7.7_mps;
     constexpr double kThetaDeadzone = 1.0;
     constexpr double kThetaControllerDefaultP = 0.13;
